f13080324ca109898e747d54314fe37d7d04681d
MartinKrutil <martin.krutil@email.cz>
2024-03-27 22:41:33 +0100

    grid class + idk

--- snake.py
+++ snake.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 pygame.display.set_caption("snake")
@@ -7,49 +7,66 @@ clock = pygame.time.Clock()
 delta_time = 0
 is_running = True
 
-background_color = pygame.Color(60, 240, 140)
-font = pygame.font.Font(None, 50)
-fps_surface = font.render("0", False, "red")
+# font = pygame.font.Font(None, 50)
+# fps_surface = font.render("0", False, "red")
 
-# snake class
+background_color = pygame.Color(60, 240, 140)
+
+default_snake_color = pygame.Color(60, 240, 140)
+default_food_color = pygame.Color(230, 80, 80)
+
+class Grid:
+    width: int
+    length: int 
+    cells: any
+
+    def __init__(self, width: int, length: int):
+        self.width = width
+        self.length = length
+        self.cells = [width][length]
+
 class Snake:
-    body = list()
-    direction = tuple()
-    color = pygame.Color(60, 240, 140)
+    body: list
+    direction: tuple
+    color: pygame.Color
+
     def __init__(self, position: tuple, direction: tuple, color: pygame.Color):
-        self.body.append(position)
+        self.body = {position}
         self.direction = direction
         self.color = color
 
+    def move(direction: tuple): pass
+    def grow(value: int): pass
+
 class Food:
-    value: 1 
-    position = tuple()
-    color = pygame.Color(230, 80, 80)
-    def __init__(self, value: int, position: tuple, color: pygame.Color):
-        self.value = value
+    position: tuple
+    value: int 
+    color: pygame.Color
+
+    def __init__(self, position: tuple, value: int, color: pygame.Color):
         self.position = position
+        self.value = value
         self.color = color
 
-#  snake = Snake()
+grid = Grid(30, 30)
+snake = Snake((20,20), (0,1), default_snake_color)
+food = Food((5,5), 1, default_food_color)
 
 # game loop
-while is_running:
+while True:
     for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: is_running = False
+        if event.type == pygame.QUIT: 
+            pygame.quit()
+            sys.exit()
 
         screen.fill(background_color)
         
         # rect = pygame.Rect(snake_p)
-
-        pygame.draw.circle()
-
-        fps_surface = font.render(str(clock.get_fps()), False, "red")
-        screen.blit(fps_surface, (0,0))
+        # pygame.draw.circle()
+        # fps_surface = font.render(str(clock.get_fps()), False, "red")
+        # screen.blit(fps_surface, (0,0))
 
         pygame.display.update()
 
         # will use for framerate independent movement later
         delta_time = clock.tick() / 1000
-
-
-pygame.quit()
