e5787f4b6f9201526f86d77997cb7a677c30f4ae
MartinKrutil <martin.krutil@email.cz>
2024-04-02 23:09:17 +0200

    added timed update event and input queue

--- src/game.py
+++ src/game.py
@@ -7,9 +7,10 @@ class Game:
     grid = objects.Grid
     snake: objects.Snake
     food: objects.Food
+    input_queue: list
     screen = pygame.Surface
     clock = pygame.time.Clock
-    grow_for = 5
+    update_timer_event: pygame.event.Event
 
     def __init__(self):
         pygame.init()
@@ -17,6 +18,7 @@ class Game:
         self.grid = objects.Grid(30, 30, 10)
         self.screen = pygame.display.set_mode((self.grid.width * self.grid.cell_size, self.grid.length * self.grid.cell_size))
         self.clock = pygame.time.Clock()
+        self.input_queue = list()
         self.food = objects.Food(
                 value = 1, 
                 size = self.grid.cell_size,
@@ -30,17 +32,22 @@ class Game:
             direction = objects.Direction.UP,
             color = pygame.Color(77, 166, 255)
         )
+        self.update_timer_event = pygame.event.custom_type()
+        pygame.time.set_timer(self.update_timer_event, 100)
+        pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, self.update_timer_event])
 
     def run(self):
         self.check_collisions()
         self.handle_events()
-        self.update()
+        self.clock.tick()
 
     def update(self):
-        self.clock.tick(10)
-        self.snake.move()
         self.screen.fill(pygame.Color(60, 240, 140))
         self.food.draw(self.screen)
+        self.snake.move()
+        if self.input_queue: 
+            self.snake.direction = self.input_queue[0]
+            self.input_queue.pop(0)
         self.snake.draw(self.screen)
         pygame.display.update()
 
@@ -61,6 +68,7 @@ class Game:
     def handle_events(self):
         for event in pygame.event.get():
             match event.type:
+                case self.update_timer_event: self.update()
                 case pygame.KEYDOWN: self.handle_controls(event.key)
                 case pygame.QUIT: self.exit_game()
 
@@ -68,20 +76,20 @@ class Game:
         match key:
             case pygame.K_UP | pygame.K_w: 
                 if (self.snake.direction != objects.Direction.DOWN):
-                    self.snake.direction = objects.Direction.UP
-            case pygame.K_LEFT | pygame.K_a: 
+                    self.input_queue.append(objects.Direction.UP)
+            case pygame.K_LEFT | pygame.K_a:
                 if (self.snake.direction != objects.Direction.RIGHT):
-                    self.snake.direction = objects.Direction.LEFT
+                    self.input_queue.append(objects.Direction.LEFT)
             case pygame.K_DOWN | pygame.K_s: 
                 if (self.snake.direction != objects.Direction.UP):
-                    self.snake.direction = objects.Direction.DOWN
+                    self.input_queue.append(objects.Direction.DOWN)
             case pygame.K_RIGHT | pygame.K_d: 
                 if (self.snake.direction != objects.Direction.LEFT):
-                    self.snake.direction = objects.Direction.RIGHT
+                    self.input_queue.append(objects.Direction.RIGHT)
 
     def game_over(self):
-        pass
+        self.exit_game()
 
-    def exit_game():
+    def exit_game(self):
         pygame.quit()
         exit()
